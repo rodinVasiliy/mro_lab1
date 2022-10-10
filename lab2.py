f9243ae4feb44e58a7d93fb4fe97ef5662fcead1
Василий Родин <varodg2000@mail.ru>
2022-10-10 21:19:20 +0300

    getting bayesian border for normal classes with the different correlation matrix function is implemented

--- lab2.py
+++ lab2.py
@@ -26,7 +26,7 @@ def get_bayesian_border_for_normal_classes_with_same_cor_matrix(X, M_l, M_j, B, 
 
 
 def get_bayesian_border_for_normal_classes(X, M_l, M_j, B_l, B_j, P_l, P_j):
-    Y = []
+    bound = []
 
     B_j_inv = np.linalg.inv(B_j)
     B_l_inv = np.linalg.inv(B_l)
@@ -43,11 +43,14 @@ def get_bayesian_border_for_normal_classes(X, M_l, M_j, B_l, B_j, P_l, P_j):
         B = (b_matrix_1[0, 1] + b_matrix_1[1, 0]) * x + b_matrix_2[0, 1]
         C = b_matrix_1[0, 0] * (x ** 2) + b_matrix_2[0, 0] * x + c
         D = (B ** 2) - 4 * A * C
-        y1 = ((-1) * B + np.sqrt(D)) / (2 * A)
-        y2 = ((-1) * B - np.sqrt(D)) / (2 * A)
-        Y.append(np.float_(y1))
-
-    return np.array(Y)
+        if D >= 0:
+            y1 = ((-1) * B + np.sqrt(D)) / (2 * A)
+            y2 = ((-1) * B - np.sqrt(D)) / (2 * A)
+            if y1 == y2:
+                bound += [x, y1.item()]
+            else:
+                bound += [[x, y1.item()], [x, y2.item()]]
+    return np.array(bound)
 
 
 if __name__ == '__main__':
@@ -66,4 +69,18 @@ if __name__ == '__main__':
     plt.plot(x_border, y_border, color='green')
     show_vector_points(feature1, 'red')
     show_vector_points(feature2, 'blue')
+    plt.show()
+    feature1, feature3, feature4 = load_features('C:\\mro_lab1\\three_classes.npy')
+    x_border = np.linspace(-3, 2, 200)
+    bound_1_3 = get_bayesian_border_for_normal_classes(x_border, M1, M2, R1, R2, P1, P2)
+    bound_1_4 = get_bayesian_border_for_normal_classes(x_border, M1, M3, R1, R3, P1, P2)
+    bound_3_4 = get_bayesian_border_for_normal_classes(x_border, M2, M3, R2, R3, P1, P2)
+    plt.ylim(-4, 3)
+    plt.xlim(-3, 2)
+    plt.scatter(bound_1_3[:, 0], bound_1_3[:, 1], color='orange')
+    plt.scatter(bound_1_4[:, 0], bound_1_4[:, 1], color='black')
+    plt.scatter(bound_3_4[:, 0], bound_3_4[:, 1], color='yellow')
+    show_vector_points(feature1, 'red')
+    show_vector_points(feature3, 'blue')
+    show_vector_points(feature4, 'green')
     plt.show()
