6168ba6d7ceb4207ed459f6e7e09fd3489b47eac
Василий Родин <varodg2000@mail.ru>
2022-11-09 23:50:38 +0400

    constants added

---
+++ Constants.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+M1 = np.array([0, 0]).reshape(2, 1)
+M2 = np.array([1, 1]).reshape(2, 1)
+M3 = np.array([-1, 1]).reshape(2, 1)
+R1 = np.array(([0.11, 0.0], [0.0, 0.19]))
+R2 = np.array(([0.23, 0.01], [0.02, 0.17]))
+R3 = np.array(([0.2, 0.1], [0.1, 0.3]))
+P1 = 0.5
+P2 = 0.5
+P3 = 0.5
